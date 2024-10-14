00099d433b409dec163974eb037e719fb489fcc0
Marcos Júnior <marcosajr11@gmail.com>
2024-10-14 17:21:17 -0300

    Classes done

---
+++ src/main/java/model/iPhone.java
@@ -0,0 +1,52 @@
+package model;
+
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    //AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo a chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando o correio de voz");
+    }
+
+    //NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    //ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+}
